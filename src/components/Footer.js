d10e8568c5a6d5d43981af9b825f796003a03feb
Tejaswini <Tejaswini.Angadi@sony.com>
2022-09-22 14:24:13 +0530

    React App

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-center'>
+            &copy; 2022 - All rights reserved by{' '}
+            <h5>Tejaswini</h5>
+        </div>
+    );
+};
+
+export default Footer;
